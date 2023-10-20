dbb841003bd9650461294be6d12c541dac1fd435
GopalAror <punjabijanu586@gmail.com>
2023-10-20 10:28:55 +0530

    feedback

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,21 +36,20 @@ menu.addEventListener("click",function(){
     span6.classList.remove("span3");
   })
   $('.slider').slick({
-    slidesToShow: 6,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 0,
     cssEase:'linear',
     speed:4000,
     Infinity:true,
-    rtl:true,
-    prevArrow:'.prev',
-    nextArrow:'.next',
+    prevArrow:false,
+    nextArrow:false,
     responsive: [
       {
         breakpoint: 1200,
         settings: {
-          slidesToShow: 4,
+          slidesToShow: 5,
           slidesToScroll: 1,
           infinite: true,
         }
@@ -58,55 +57,62 @@ menu.addEventListener("click",function(){
       {
         breakpoint: 992,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 4,
           slidesToScroll: 1
         }
       },
       {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-  $('.slider2').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 0,
-    cssEase:'linear',
-    speed:4000,
-    Infinity:true,
-    prevArrow:'.prev',
-    nextArrow:'.next',
-    responsive: [
-      {
-        breakpoint: 1200,
+        breakpoint: 768,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
         breakpoint: 576,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1
         }
       }
     ]
   });
+  // $('.slider2').slick({
+  //   slidesToShow: 6,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 0,
+  //   cssEase:'linear',
+  //   speed:4000,
+  //   Infinity:true,
+  //   prevArrow:'.prev',
+  //   nextArrow:'.next',
+  //   responsive: [
+  //     {
+  //       breakpoint: 1200,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 3,
+  //         infinite: true,
+  //         dots: true
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 992,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 576,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1
+  //       }
+  //     }
+  //   ]
+  // });
   $('.slider5').slick({
     infinite: true,
     speed: 500,
